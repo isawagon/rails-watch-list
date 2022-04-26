7cd9d304e53b2dd1596f96191a00e0eadb2c0c9c
isabelle levy <isabel.levy@icloud.com>
2022-04-26 18:19:30 +0200

    lists

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,26 @@
 class BookmarksController < ApplicationController
+
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    if @bookmark.save
+      redirect_to list_path(@bookmark)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to bookmarks_path
+  end
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:movie_id, :list_id, :comment)
+  end
 end
--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,5 +2,5 @@ class Bookmark < ApplicationRecord
   validates :comment, length: { minimum: 6 }
   belongs_to :movie
   belongs_to :list
-  validates :movie, uniqueness: { scope: :list}
+  validates :movie_id, uniqueness: { scope: :list_id, message: "is already in the list"}
 end
